4815784952727e925d004f17b064efb8d51ce141
敖律风 <x123456789fy@outlook.com>
2024-09-23 15:38:13 +0800

    add: 命令行模式等待用户输入 fix: actions 部分流程未写路径 fix: 让上传 release 文件等待其他任务执行完毕

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"flag"
-	"github.com/gookit/color"
+	"fmt"
 	"github.com/rs/zerolog"
 	"os"
 	"time"
@@ -24,13 +24,20 @@ func main() {
 	clientSetup := &client{*addr, *port, *password}
 	switch *launchType {
 	case "exec":
+		for *command == "" {
+			log.Info().Msg("no command input, please input command:")
+			_, err := fmt.Scanln(*command)
+			if err != nil {
+				log.Fatal().AnErr("command error:", err).Msgf("can't read command")
+			}
+		}
 		result, err := execCommand(clientSetup, command)
 		if err != nil {
 			log.Fatal().AnErr("exec error:", err).Msgf("can't execute command")
 		}
 		log.Info().Msgf("result: %s", result)
 	case "console":
-		color.BgBlack.Print(" ______ __  __   __  __  __  ______  ______  ______  __   __  ______  __      __  ______  __   __  ______  \n/\\__  _/\\ \\/\\ \"-.\\ \\/\\ \\_\\ \\/\\  == \\/\\  ___\\/\\  __ \\/\\ \"-.\\ \\/\\  ___\\/\\ \\    /\\ \\/\\  ___\\/\\ \"-.\\ \\/\\__  _\\ \n\\/_/\\ \\\\ \\ \\ \\ \\-.  \\ \\____ \\ \\  __<\\ \\ \\___\\ \\ \\/\\ \\ \\ \\-.  \\ \\ \\___\\ \\ \\___\\ \\ \\ \\  __\\\\ \\ \\-.  \\/_/\\ \\/ \n   \\ \\_\\\\ \\_\\ \\_\\\\\"\\_\\/\\_____\\ \\_\\ \\_\\ \\_____\\ \\_____\\ \\_\\\\\"\\_\\ \\_____\\ \\_____\\ \\_\\ \\_____\\ \\_\\\\\"\\_\\ \\ \\_\\ \n    \\/_/ \\/_/\\/_/ \\/_/\\/_____/\\/_/ /_/\\/_____/\\/_____/\\/_/ \\/_/\\/_____/\\/_____/\\/_/\\/_____/\\/_/ \\/_/  \\/_/ \n")
+		motd()
 		err := newSession(*clientSetup)
 		if err != nil {
 			log.Warn().AnErr("session error:", err).Msgf("")
